2c55c9beb590e6f72114815f4cf0428cf46754b9
Adrien <gaarf@users.noreply.github.com>
2023-05-24 14:11:32 -0700

    rm preprocessReact

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,13 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import preprocessReact from "svelte-preprocess-react/preprocessReact";
+// import preprocessReact from "svelte-preprocess-react/preprocessReact";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: preprocessReact({
+
+	// preprocess: preprocessReact({
     preprocess: vitePreprocess(),
-  }),
+  // }),
 
 	kit: {
 		adapter: adapter()
