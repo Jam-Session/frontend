9b777e19da5b61c212797b8b1c5e2d164b7b7738
Adrien <gaarf@users.noreply.github.com>
2023-05-28 16:05:50 -0700

    geolocation.getCurrentPosition

--- src/routes/jams/+page.svelte
+++ src/routes/jams/+page.svelte
@@ -1,1 +1,19 @@
-hello jams
+<script lang="ts">
+	import { browser } from '$app/environment';
+
+	let where: number[];
+
+	if (browser) {
+		navigator.geolocation.getCurrentPosition(({ coords }) => {
+			where = [coords.latitude, coords.longitude];
+		});
+	}
+</script>
+
+<div class="container p-4">
+	{#if where}
+		<pre class="pre">{JSON.stringify(where)}</pre>
+	{:else}
+		<p class="animate-bounce">Please allow geolocation...</p>
+	{/if}
+</div>
