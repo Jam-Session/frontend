b213579acde09aaed06a4993e77acf6b54cc5316
Adrien <gaarf@users.noreply.github.com>
2023-06-10 17:54:46 -0700

    nice

---
+++ src/lib/DefList.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let obj: Record<string | number, unknown>;
+</script>
+
+<dl class="grid grid-cols-2">
+	{#each Object.entries(obj) as [key, val]}
+		<dt class="font-mono mr-4">{key}</dt>
+		<dd class="text-right">{val}</dd>
+	{/each}
+</dl>
--- src/routes/satocast/+page.svelte
+++ src/routes/satocast/+page.svelte
@@ -1,3 +1,40 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { PageData } from './$types';
+	import { addSeconds, format } from 'date-fns';
+	import DefList from '$lib/DefList.svelte';
+
+	export let data: PageData;
+	let index = 0;
+	let when = new Date(data.ohlc[index].time);
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			const next = data.ohlc[index + 1];
+			if (next) {
+				when = addSeconds(when, 60 * 60);
+				if (when.valueOf() >= next.time) {
+					index += 1;
+				}
+			}
+			else {
+				clearInterval(interval);
+			}
+		}, 200);
+		return () => clearInterval(interval);
+	});
+
+	$: candles = data.ohlc.slice(0, index + 1);
+</script>
+
 <div class="container p-4">
-  hello world
+	<p class="chip variant-ringed mb-4">{format(when, 'PPppp')}</p>
+	<ol class="flex flex-wrap gap-2">
+		{#each candles as { time, ...ohlc }}
+			<li class="variant-ringed p-2 rounded">
+				<h6 class="border-b border-surface-500 text-center mb-2">{format(time, 'pp')}</h6>
+				<DefList obj={ohlc} />
+			</li>
+		{/each}
+	</ol>
 </div>
