8bd619a672b5bdfd9d9a9b5c1c9c44a7d532be31
Adrien <gaarf@users.noreply.github.com>
2023-06-17 16:28:09 -0700

    argj

--- prisma/seed/index.cjs
+++ prisma/seed/index.cjs
@@ -1,6 +1,6 @@
 async function main() {
 	const { PrismaClient } = await import('@prisma/client');
-	const { seed } = await import('../src/routes/satocast/seed/candles.js');
+	const { seed } = await import('./candles.js');
 	
 	const prisma = new PrismaClient({
 		log: ['info', 'error', 'warn']
--- src/routes/satocast/seed/+server.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from '$lib/db';
-import { json } from '@sveltejs/kit';
-import { seed } from './candles';
-
-export const GET = async () => {
-    await seed(prisma);
-    const count = await prisma.candle.count();
-    return json({ count });
-};
